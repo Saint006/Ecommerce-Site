4a18903df7a1e686b6801e1a6fdf696889fd58fb
Saint006 <saintunni1306@gmail.com>
2025-02-16 11:54:42 +0530

    Models Created

--- customers/models.py
+++ customers/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Product(models.Model):
+class Customer(models.Model):
     LIVE=1
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
-    title=models.CharField(max_length=200)
-    price=models.FloatField()
-    description=models.TextField()
-    image=models.ImageField()
-    priority =  models.IntegerField(default=0)
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    phone=models.CharField(max_length=10)
+    address=models.TextField()
+    user=models.OneToOneField(User,related_name='customer_profile',on_delete=models.CASCADE) 
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
